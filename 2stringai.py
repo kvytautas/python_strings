58aa1c53635e42429d43bfdb14c31c90d6fa684f
kvytautas <7vytautas7@gmail.com>
2026-02-17 14:46:02 +0200

    Initial commit

---
+++ 2stringai.py
@@ -0,0 +1,12 @@
+print ("*********** Užduotis1 ************")
+
+name="Johnny"
+last_name="Depp"
+
+if len(name)>len(last_name):
+    print(name)
+elif len(name)<len(last_name):
+    print(last_name)
+else: print(name, last_name)
+
+print ("*********** Užduotis2 ************")
